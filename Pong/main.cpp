e36bc47bfa4646671871e95494079189fa961cad
Joe Corrales <corralesjoe07@gmail.com>
2022-04-11 16:51:23 -0600

    jsjjs

--- Pong/main.cpp
+++ Pong/main.cpp
@@ -1,4 +1,6 @@
 #include "raylib.h"
+#include <iostream>
+
 
 struct Ball {
 	float x, y;
@@ -16,7 +18,11 @@ struct Paddle {
 	float width, height;
 
 	void Draw() {
-		DrawRectangle(x - width / 2, y - height / 2, 10, 100, WHITE);
+		DrawRectangleRec(GetRect(), WHITE);
+	}
+
+	Rectangle GetRect() {
+		return Rectangle{ x - width / 2, y - height / 2, 10, 100 };
 	}
 };
 
@@ -31,8 +37,8 @@ int main()
 	ball.x = GetScreenWidth() / 2.0f;
 	ball.y = GetScreenHeight() / 2.0f;
 	ball.radius = 5.0f;
-	ball.speedX = 150;
-	ball.speedY = 150;
+	ball.speedX = 300;
+	ball.speedY = 300;
 
 	Paddle leftPaddle;
 	leftPaddle.x = 50;
@@ -47,6 +53,9 @@ int main()
 	rightPaddle.width = 10;
 	rightPaddle.height = 100;
 	rightPaddle.speedY = 500;
+
+	const char* winnerText = nullptr;
+
 
 	while (!WindowShouldClose()) 
 	{
@@ -63,12 +72,71 @@ int main()
 			ball.speedY *= -1;
 		}
 
+		if (IsKeyDown(KEY_W)) {
+			std::cout << "Coordenada y: " << leftPaddle.y << "\n";
+			leftPaddle.y -= leftPaddle.speedY * GetFrameTime();
+		}
+
+		if (IsKeyDown(KEY_S)) {
+			std::cout << "Coordenada y: " << leftPaddle.y << "\n";
+			leftPaddle.y += leftPaddle.speedY * GetFrameTime();
+		}
+
+		if (IsKeyDown(KEY_UP)) {
+			rightPaddle.y -= rightPaddle.speedY * GetFrameTime();
+		}
+
+		if (IsKeyDown(KEY_DOWN)) {
+			rightPaddle.y += rightPaddle.speedY * GetFrameTime();
+		}
+
+		if (CheckCollisionCircleRec(Vector2{ ball.x, ball.y }, ball.radius,
+			leftPaddle.GetRect()))
+		{
+			if (ball.speedX < 0) {
+				ball.speedX *= -1.1f;
+				ball.speedY = (ball.y - leftPaddle.y) / (leftPaddle.height / 2) * ball.speedX;
+			}
+		}
+
+		if (CheckCollisionCircleRec(Vector2{ ball.x, ball.y }, ball.radius,
+			rightPaddle.GetRect())) 
+		{
+			if (ball.speedX > 0) 
+			{
+				ball.speedX *= -1.1f;
+				ball.speedY = (ball.y - rightPaddle.y) / (rightPaddle.height / 2) * -ball.speedX;
+			}
+		}
+
+		if (ball.x < 0) {
+			winnerText = "Right Player Wins!";
+		}
+
+		if (ball.x > GetScreenWidth()) {
+			winnerText = "Left Player Wins!";
+		}
+
+		if (winnerText && IsKeyPressed(KEY_SPACE)) {
+			ball.x = GetScreenWidth() / 2;
+			ball.y = GetScreenHeight() / 2;
+			ball.speedX = 300;
+			ball.speedY = 300;
+			winnerText = nullptr;
+		}
+
 		BeginDrawing();
 		ClearBackground(BLACK);
 
 		ball.Draw();
 		leftPaddle.Draw();
 		rightPaddle.Draw();
+
+		if (winnerText) {
+			int textWidth = MeasureText(winnerText, 60);
+			DrawText(winnerText, GetScreenWidth() / 2 - textWidth /2  , GetScreenHeight() / 2 - 30, 60, YELLOW);
+		}
+
 
 		DrawFPS(10, 10);
 		EndDrawing();
